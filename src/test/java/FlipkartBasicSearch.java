410c37bdeb88bca77ad25e360e0dcff204ce7122
nairgirish <47943802+nairgirish@users.noreply.github.com>
2020-08-08 14:03:16 +0530

    added comments

--- src/test/java/FlipkartBasicSearch.java
+++ src/test/java/FlipkartBasicSearch.java
@@ -15,19 +15,19 @@ public class FlipkartBasicSearch {
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
-		driver.get("https://www.flipkart.com");
+		driver.get("https://www.flipkart.com"); //Browser launched
 		
 	}
 
 	@Test
 	public void searchProduct() {
 		
-		System.out.println(driver.getTitle());
+		System.out.println(driver.getTitle()); //Print page title
 		Actions action = new Actions(driver);
 		action.moveToElement(driver.findElement(By.xpath("//div[@class='_3Njdz7']"))).build().perform();
 		driver.findElement(By.xpath("//div[@class='_3Njdz7']//button[@class='_2AkmmA _29YdH8']")).click();
-		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Speakers");
-		driver.findElement(By.xpath("//button[@type='submit']")).click();
+		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Speakers"); //Enter product
+		driver.findElement(By.xpath("//button[@type='submit']")).click(); //Click Search
 
 	}
 	
