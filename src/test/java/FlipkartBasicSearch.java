7656c74cac403811c2eb08dc9ddb3d0a4a550085
nairgirish <47943802+nairgirish@users.noreply.github.com>
2020-08-08 13:53:52 +0530

    updated file to testng

--- src/test/java/FlipkartBasicSearch.java
+++ src/test/java/FlipkartBasicSearch.java
@@ -7,23 +7,33 @@ import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class FlipkartBasicSearch {
-
-	public static void main(String[] args) {
+	
+	@BeforeMethod
+	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "D:\\Selenium-Udemy\\chromedriver_win32\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().pageLoadTimeout(25, TimeUnit.SECONDS);
 		driver.get("https://www.flipkart.com");
+		
+	}
+
+	@Test
+	public void searchProduct() {
+		
 		System.out.println(driver.getTitle());
 		Actions action = new Actions(driver);
 		action.moveToElement(driver.findElement(By.xpath("//div[@class='_3Njdz7']"))).build().perform();
 		driver.findElement(By.xpath("//div[@class='_3Njdz7']//button[@class='_2AkmmA _29YdH8']")).click();
-		WebDriverWait wait= new WebDriverWait(driver,30);
-//		wait.until(ExpectedConditions.visibilityOf(element));
 		driver.findElement(By.xpath("//input[@type='text']")).sendKeys("Speakers");
 		driver.findElement(By.xpath("//button[@type='submit']")).click();
 
+	}
+	
+	@AfterMethod
+	public void tearDown() {
+	driver.quit()
 	}
 
 }
